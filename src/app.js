52a344bb5bf7cad1344891c139736968b18b1835
Nick Hoszko <nhoszko@gmail.com>
2021-07-28 15:33:12 +1000

    rm dummy data

--- src/app.js
+++ src/app.js
@@ -11,13 +11,6 @@ import './styles/styles.scss';
  
 const store = configureStore();
 
-
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-
-
-console.log(visibleExpenses);
-
 const jsx = (
   <Provider store={store}>
     <AppRouter />
