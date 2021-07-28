d5c16c611b35d8b94dda5f6eae0219bc66e09bd6
Nick Hoszko <nhoszko@gmail.com>
2021-07-28 16:34:48 +1000

    setup production webpack build

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ import {setTextFilter} from './action-generators/filters';
 import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css'
 import './styles/styles.scss';
+import 'react-dates/lib/css/_datepicker.css'
  
 const store = configureStore();
 
