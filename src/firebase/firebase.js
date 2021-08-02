3f45a20f4f049654d5403dc73e54aa0bb9a92c5d
Nick Hoszko <nhoszko@gmail.com>
2021-08-03 08:58:42 +1000

    Setup test datbase env

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,12 +2,12 @@
 import * as firebase from 'firebase';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAhqALO2hiavBjzOpM8GiOystF6oIYHMJI",
-  authDomain: "expensify-7f66d.firebaseapp.com",
-  databaseURL: "https://expensify-7f66d-default-rtdb.firebaseio.com",
-  projectId: "expensify-7f66d",
-  storageBucket: "expensify-7f66d.appspot.com",
-  messagingSenderId: "453775131419",
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
   appId: "1:453775131419:web:a42343775f9adc373749be",
   measurementId: "G-09EWFGJWKB"
 };
